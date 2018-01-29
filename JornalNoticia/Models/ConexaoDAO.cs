63dd0f2e0983abe99904e2a8f1092963c11bce0c
mat5855 <mateusrreis5855@hotmail.com>
2018-01-29 16:33:36 -0200

    Atualização do Projeto Bootstrap

--- JornalNoticia/Models/ConexaoDAO.cs
+++ JornalNoticia/Models/ConexaoDAO.cs
@@ -13,12 +13,12 @@ namespace JornalNoticia.Models
         public int inserir(Noticia noticia)
         {
             int numerodelinhas = 0;
-            bdConn = new MySqlConnection("Server=localhost;Database=teste;Uid=root;Pwd=gengar38");
+            bdConn = new MySqlConnection("Server=localhost;Database=noticia;Uid=root;Pwd=necnec@1");
             try
             {
                 bdConn.Open();
                 
-                MySqlCommand cmd = new MySqlCommand("insert into noticia(titulo,publicacao,caminhoimg)  values(@Titulo,@Publicacao,@Caminho)", bdConn);
+                MySqlCommand cmd = new MySqlCommand("insert into publicacao(titulo,corponoticia,caminhoimg)  values(@Titulo,@Publicacao,@Caminho)", bdConn);
                 cmd.Parameters.AddWithValue("@Titulo", noticia.Titulo);
                 cmd.Parameters.AddWithValue("@Publicacao", noticia.Corponoticia);
                 cmd.Parameters.AddWithValue("@Caminho", noticia.caminhoimg);
@@ -47,13 +47,13 @@ namespace JornalNoticia.Models
         public List<Noticia> consultarNoticia()
         {
            
-            bdConn = new MySqlConnection("Server=localhost;Database=teste;Uid=root;Pwd=gengar38");
+            bdConn = new MySqlConnection("Server=localhost;Database=noticia;Uid=root;Pwd=necnec@1");
             List<Noticia> listadados = new List<Noticia>();
             try
             {
                 bdConn.Open();
 
-                MySqlCommand cmd = new MySqlCommand("select * from noticia LIMIT 3", bdConn);
+                MySqlCommand cmd = new MySqlCommand("select * from publicacao LIMIT 3", bdConn);
                 MySqlDataReader reader = cmd.ExecuteReader();
                 
                 while(reader.Read())
@@ -61,7 +61,7 @@ namespace JornalNoticia.Models
                     Noticia noticia = new Noticia();
                     noticia.Idnoticia = Convert.ToInt32(reader["idnoticia"].ToString());
                     noticia.Titulo = Convert.ToString(reader["titulo"].ToString());
-                    noticia.Corponoticia = Convert.ToString(reader["publicacao"].ToString());
+                    noticia.Corponoticia = Noticia.TruncateString(Convert.ToString(reader["corponoticia"].ToString()),40,Noticia.TruncateOptions.None);
                     noticia.caminhoimg = Convert.ToString(reader["caminhoimg"].ToString());
                     listadados.Add(noticia);
                 }
